f4335879a81eb931388c0782d60abf65a2c42798
nosdvp <mishchenkoa.97@gmail.com>
2026-09-15 14:52:05 +0300

    - Created a modal to display the winner - Added logic to determine the winning combination - Added a round-by-round win counter for players - Added modal display upon winning

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import cross from './img/cross.svg'
 import circle from './img/circle.svg'
 import reset from './img/reset.svg'
-import crossSilver from './img/cross-silver.svg'
-import circleSilver from './img/circle-silver.svg'
 
 function App() {
 
@@ -16,6 +14,10 @@ function App() {
 
   const [winner, setWinner] = useState('')
   const [modalWin, setModalWin] = useState(false)
+
+  const [countWinCross, setCountWinCross] = useState(0)
+  const [countWinCircle, setCountWinCircle] = useState(0)
+  const [countTies, setCountTies] = useState(0)
 
   const [field, setField] = useState([
     '', '', '',
@@ -35,75 +37,94 @@ function App() {
 
     currentStep === 'cross' ? setCurrentStep('circle') : setCurrentStep('cross')
 
-    if(field[0] === 'cross' && field[1] === 'cross' && field[2] === 'cross'){
+    if(newField[0] === 'cross' && newField[1] === 'cross' && newField[2] === 'cross'){
       setWinner('cross')
-      setModalWin('true')
+      setModalWin(true)
     }
 
-    if(field[3] === 'cross' && field[4] === 'cross' && field[5] === 'cross'){
+    if(newField[3] === 'cross' && newField[4] === 'cross' && newField[5] === 'cross'){
       setWinner('cross')
-      setModalWin('true')
+      setModalWin(true)
     }
 
-    if(field[6] === 'cross' && field[7] === 'cross' && field[8] === 'cross'){
+    if(newField[6] === 'cross' && newField[7] === 'cross' && newField[8] === 'cross'){
       setWinner('cross')
-      setModalWin('true')
+      setModalWin(true)
     }
 
     if(newField[0] === 'cross' && newField[3] === 'cross' && newField[6] === 'cross'){
       setWinner('cross')
-      setModalWin('true')
+      setModalWin(true)
     }
 
-    if(field[1] === 'cross' && field[4] === 'cross' && field[7] === 'cross'){
+    if(newField[1] === 'cross' && newField[4] === 'cross' && newField[7] === 'cross'){
       setWinner('cross')
-      setModalWin('true')
+      setModalWin(true)
     }
 
-    if(field[2] === 'cross' && field[5] === 'cross' && field[8] === 'cross'){
+    if(newField[2] === 'cross' && newField[5] === 'cross' && newField[8] === 'cross'){
       setWinner('cross')
-      setModalWin('true')
+      setModalWin(true)
     }
 
-    if(field[0] === 'cross' && field[4] === 'cross' && field[8] === 'cross'){
+    if(newField[0] === 'cross' && newField[4] === 'cross' && newField[8] === 'cross'){
       setWinner('cross')
-      setModalWin('true')
+      setModalWin(true)
     }
 
-    if(field[0] === 'circle' && field[1] === 'circle' && field[2] === 'circle'){
-      setWinner('circle')
-      setModalWin('true')
+    if(newField[2] === 'cross' && newField[4] === 'cross' && newField[6] === 'cross'){
+      setWinner('cross')
+      setModalWin(true)
     }
 
-    if(field[3] === 'circle' && field[4] === 'circle' && field[5] === 'circle'){
+    if(newField[0] === 'circle' && newField[1] === 'circle' && newField[2] === 'circle'){
       setWinner('circle')
-      setModalWin('true')
+      setModalWin(true)
     }
 
-    if(field[6] === 'circle' && field[7] === 'circle' && field[8] === 'circle'){
+    if(newField[3] === 'circle' && newField[4] === 'circle' && newField[5] === 'circle'){
       setWinner('circle')
-      setModalWin('true')
+      setModalWin(true)
     }
 
-    if(field[0] === 'circle' && field[3] === 'circle' && field[6] === 'circle'){
+    if(newField[6] === 'circle' && newField[7] === 'circle' && newField[8] === 'circle'){
       setWinner('circle')
-      setModalWin('true')
+      setModalWin(true)
     }
 
-    if(field[1] === 'circle' && field[4] === 'circle' && field[7] === 'circle'){
+    if(newField[0] === 'circle' && newField[3] === 'circle' && newField[6] === 'circle'){
       setWinner('circle')
-      setModalWin('true')
+      setModalWin(true)
     }
 
-    if(field[2] === 'circle' && field[5] === 'circle' && field[8] === 'circle'){
+    if(newField[1] === 'circle' && newField[4] === 'circle' && newField[7] === 'circle'){
       setWinner('circle')
-      setModalWin('true')
+      setModalWin(true)
     }
 
-    if(field[0] === 'circle' && field[4] === 'circle' && field[8] === 'circle'){
+    if(newField[2] === 'circle' && newField[5] === 'circle' && newField[8] === 'circle'){
       setWinner('circle')
-      setModalWin('true')
+      setModalWin(true)
     }
+
+    if(newField[0] === 'circle' && newField[4] === 'circle' && newField[8] === 'circle'){
+      setWinner('circle')
+      setModalWin(true)
+    }
+
+    if(newField[2] === 'cross' && newField[4] === 'cross' && newField[6] === 'cross'){
+      setWinner('cross')
+      setModalWin(true)
+    }
+  }
+
+  const gameReset = () => {
+    setField([
+      '', '', '',
+      '', '', '',
+      '', '', '',
+    ])
+    setCurrentStep('cross')
   }
 
   return (
@@ -151,6 +172,45 @@ function App() {
         </>
       ) : (
         <>
+          {modalWin === true && (
+            <div className='modal'>
+              <div className='modal__infoBlock'>
+                  <p className='modal__infoBlock_title'>YOU WON!</p>
+                  <p className='modal__infoBlock_result'>{winner === 'cross' ? <img src={cross}/> : <img src={circle}/>} TAKES THE ROUND</p>
+                  <div className='modal__infoBlock_navBlock'>
+                    <button onClick={() => {
+                      setGameStep('menu')
+                      setModalWin(false)
+                      setField([
+                        '', '', '',
+                        '', '', '',
+                        '', '', '',
+                      ])
+                      setCurrentStep('cross')
+                      setCountWinCircle(0)
+                      setCountWinCross(0)
+                      setCountTies(0)
+                    }}>QUIT</button>
+                    <button onClick={() => {
+                      setModalWin(false)
+                      setField([
+                        '', '', '',
+                        '', '', '',
+                        '', '', '',
+                      ])
+                      setCurrentStep('cross')
+                      if(winner === 'cross'){
+                        setCountWinCross(prev => prev + 1)
+                      }else if(winner === 'circle'){
+                        setCountWinCircle(prev => prev + 1)
+                      }else{
+                        setCountTies(prev => prev + 1)
+                      }
+                    }}>NEXT ROUND</button>
+                  </div>
+              </div>
+            </div>
+          )}
           {mode === 'play with player' ? (
             <section className='game__playField'>
 
@@ -164,7 +224,8 @@ function App() {
                   <img src={cross}/>
                   <img src={circle}/>
                 </div>
-                <button className='game__playField_nav_reset'>
+                <button className='game__playField_nav_reset' onClick={gameReset}
+                >
                   <img src={reset}/>
                 </button>
               </div>
@@ -211,18 +272,29 @@ function App() {
               <div className='game__playField_resultBlock'>
                 <div className='game__playField_resultBlock_playerResult'>
                   {gameMark === 'cross' ? <p>X (YOU)</p> : <p>O (YOU)</p>}
-                  <p>14</p>
+                  <p>{countWinCross}</p>
                 </div>
                 <div className='game__playField_resultBlock_ties'>
                   <p>TIES</p>
-                  <p>10</p>
+                  <p>{countTies}</p>
                 </div>
                 <div className='game__playField_resultBlock_opponentResult'>
                   <p>{mode === 'play with PC' ? <p>O CPU</p> : <p>PLAYER</p>}</p>
-                  <p>9</p>
+                  <p>{countWinCircle}</p>
                 </div>
               </div>
-              <button onClick={() => setGameStep('menu')} className='game__menu_back'>Back to main menu</button>
+              <button onClick={() => {
+                setGameStep('menu')
+                setField([
+                        '', '', '',
+                        '', '', '',
+                        '', '', '',
+                      ])
+                setCurrentStep('cross')
+                setCountWinCircle(0)
+                setCountWinCross(0)
+                setCountTies(0)
+              }} className='game__menu_back'>Back to main menu</button>
 
             </section>
           ) : (
